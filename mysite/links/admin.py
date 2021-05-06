345e11adfe105046d966b9ef308a9fc245d540bb
Denys Hrytseniuk <denys.hrytseniuk.work@gmail.com>
2021-05-06 12:29:57 +0300

    added new tables

--- mysite/links/admin.py
+++ mysite/links/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
 
-from .models import Links
+from .models import Links, Categories, Authors
+
 
 class LinksAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'created_at', 'count_visites')
+    list_display = ('id', 'title', 'category', 'created_at', 'count_visites')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'description')
 
 
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category_name', 'category_description')
+    list_display_links = ('id', 'category_name')
+    search_fields = ('category_name', )
+
+
+class AuthorsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'second_name', 'country', 'age')
+    list_display_links = ('id', 'first_name', 'second_name')
+    search_fields = ('first_name', 'second_name')
+
+
 admin.site.register(Links, LinksAdmin)
+admin.site.register(Categories, CategoriesAdmin)
+admin.site.register(Authors, AuthorsAdmin)
--- mysite/links/models.py
+++ mysite/links/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
+
 class Links(models.Model):
     title = models.CharField(max_length=150, verbose_name='Наименование')
     description = models.TextField(blank=True, verbose_name='Описание')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
     link_name = models.URLField(max_length=150, verbose_name='Название ссылки')
     count_visites = models.PositiveIntegerField(default=0, verbose_name='Кол-во переходов')
+    category = models.ForeignKey('Categories', on_delete=models.PROTECT, null=True)
+    author = models.ForeignKey('Authors', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.title
@@ -14,3 +17,31 @@ class Links(models.Model):
         verbose_name = 'ссылку'
         verbose_name_plural = 'Ссылки'
         ordering = ['-created_at', 'title']
+
+
+class Categories(models.Model):
+    category_name = models.CharField(max_length=150, verbose_name='Название')
+    category_description = models.TextField(blank=True, verbose_name='Описание')
+
+    def __str__(self):
+        return self.category_name
+
+    class Meta:
+        verbose_name = 'категорию'
+        verbose_name_plural = 'Категории'
+        ordering = ['category_name']
+
+
+class Authors(models.Model):
+    first_name = models.CharField(max_length=150, verbose_name='Имя')
+    second_name = models.CharField(max_length=150, verbose_name='Фамилия')
+    country = models.CharField(max_length=150, verbose_name='Страна')
+    age = models.PositiveIntegerField(verbose_name='Возраст')
+
+    def __str__(self):
+        return f'{self.first_name} {self.second_name}'
+
+    class Meta:
+        verbose_name = 'автора'
+        verbose_name_plural = 'Авторы'
+        ordering = ['first_name', 'second_name']
